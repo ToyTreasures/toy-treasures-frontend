2e6448d282491a95d94b8635a5affa0eb10d93c3
ziad <ziad.nouh111@gmail.com>
2024-09-18 16:32:10 +0300

    feat: add itemService for item api endpoints

---
+++ src/services/itemService.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api/v1/items",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const getAllItems = async () => {
+  try {
+    const res = await api.get("/");
+    return res.data;
+  } catch (error) {
+    console.log("Get all items error", error);
+  }
+};
+
+const getItemById = async (id) => {
+  try {
+    const res = await api.get(`/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log("Get item by id error", error);
+  }
+};
+
+const updateItem = async (id, data) => {
+  try {
+    const res = await api.patch(`/${id}`, data);
+    return res.data;
+  } catch (error) {
+    console.log("PATCH item error:", error);
+    throw error;
+  }
+};
+
+const creatItem = async (data) => {
+  try {
+    const res = await api.post(`/`, data);
+    return res.data;
+  } catch (error) {
+    console.log("Post item error:", error);
+    throw error;
+  }
+};
+
+const deleteItem = async (id) => {
+  try {
+    const res = await api.delete(`/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log("Delete item error", error);
+  }
+};
+
+export default {
+  getAllItems,
+  getItemById,
+  updateItem,
+  creatItem,
+  deleteItem,
+};
