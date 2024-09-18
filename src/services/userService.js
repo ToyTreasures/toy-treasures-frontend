1743d23e33349b583c4c3d5b73fface6501aa985
ziad <ziad.nouh111@gmail.com>
2024-09-18 16:09:17 +0300

    feat: add userService for user api endpoints

---
+++ src/services/userService.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api/v1/users",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const getAllUsers = async () => {
+  try {
+    const res = await api.get("/");
+    return res.data;
+  } catch (error) {
+    console.log("Get all users error", error);
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const res = await api.get(`/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log("Get user by id error", error);
+  }
+};
+
+const updateUser = async (id, data) => {
+  try {
+    const res = await api.patch(`/${id}`, data);
+    return res.data;
+  } catch (error) {
+    console.log("PATCH user error:", error);
+    throw error;
+  }
+};
+
+export default {
+  getAllUsers,
+  getUserById,
+  updateUser,
+};
