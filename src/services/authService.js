a4b25ba522d5eaea2127f27ba156cf0413962f53
ZiadNouh <64560345+ZiadNouh@users.noreply.github.com>
2024-09-19 09:34:59 +0300

    Register-login (#3)
    * chore: add formik,yup and react-router-dom
    
    * feat: add axios interceptors in authService
    
    - Add axios interceptor to include the accessToken in every request.
    - Add axios interceptor to check wheather the access token is expired or not to call the refreshToken endpoint.
    - Handled storing the accessToken and the user in the local storage on successful login.
    - Handled removing the accessToken and the user from local storage on logout.
    
    * feat: add login component
    
    * feat: add register component

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,29 +5,73 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.response.data.error ===
+        "Your token has expired, please log in again" &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshResponse = await refreshAccessToken();
+        const { accessToken } = refreshResponse;
+        localStorage.setItem("accessToken", accessToken);
+        originalRequest.headers["Authorization"] = accessToken;
+        return api(originalRequest);
+      } catch (refreshError) {
+        await logout();
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 const register = async (user) => {
   try {
     const res = await api.post("/register", user);
     return res.data;
   } catch (error) {
-    console.log("Register error", error);
+    throw error.response.data;
   }
 };
 
 const login = async (user) => {
   try {
-    const res = await api.post(`/login`, user);
+    const res = await api.post("/login", user);
+    localStorage.setItem("accessToken", res.data.accessToken);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
     return res.data;
   } catch (error) {
-    console.log("Login error", error);
+    throw error.response.data;
   }
 };
 
 const logout = async () => {
   try {
-    const res = await api.get();
+    const res = await api.post("/logout");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
     return res.data;
   } catch (error) {
     console.log("Logout error:", error);
@@ -37,7 +81,7 @@ const logout = async () => {
 
 const refreshAccessToken = async () => {
   try {
-    const res = await api.get();
+    const res = await api.post("/refresh-token");
     return res.data;
   } catch (error) {
     console.log("Refresh Access Token error:", error);
