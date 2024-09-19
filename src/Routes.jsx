e42d9cefb5a474868782f85a942041b99fb49378
moaz5071 <moaz95ibrahim@gmail.com>
2024-09-19 14:23:14 +0300

    feat: add ItemDetails page component with API fetch

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
-// import ItemDetailsPage from "./pages/ItemDetailsPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
+import ItemDetails from "./pages/ItemDetails";
 // import Signup from "./pages/Signup";
 // import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
@@ -15,9 +18,10 @@ const AppRoutes = () => {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/shop" element={<Shop />} />
-            {/* <Route path="/shop/:id" element={<ItemDetailsPage />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/shop/:id" element={<ItemDetails />} />
+            {/* <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} /> */}
             <Route path="*" element={<NotFound />} />
           </Routes>
