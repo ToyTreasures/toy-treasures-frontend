620e95573a45d57396cb4f0065605542d272bab2
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-20 07:33:24 +0200

    feat(toytypesection): add toytypesection component

---
+++ src/Layouts/HomeLayouts/HomeComponents/ToysTypeSection.jsx
@@ -0,0 +1,11 @@
+import React, { forwardRef } from "react";
+
+const ToysTypeSection = forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <h1>Toy Type Section</h1>
+    </div>
+  );
+});
+
+export default ToysTypeSection;
