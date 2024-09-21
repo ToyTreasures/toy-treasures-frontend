646bae9fa7665831f37f3bf66f183e0d47bf5d80
moaz5071 <115082396+moaz5071@users.noreply.github.com>
2024-09-21 11:26:55 +0300

    Item details (#6)
    * feat: add Profile page component
    
    * feat: add ItemDetails page component with API fetch
    
    * refactor: use backend error message
    
    ---------
    
    Co-authored-by: ZiadNouh <64560345+ZiadNouh@users.noreply.github.com>

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
-// import ItemDetailsPage from "./pages/ItemDetailsPage";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
+import ItemDetails from "./pages/ItemDetails";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import SellItem from "./pages/SellItem";
 
 const AppRoutes = () => {
@@ -18,8 +19,9 @@ const AppRoutes = () => {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/shop" element={<Shop />} />
-            {/* <Route path="/shop/:id" element={<ItemDetailsPage />} />*/}
+            <Route path="/shop/:id" element={<ItemDetails />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/sell-item" element={<SellItem />} />
