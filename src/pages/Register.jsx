267aa294b75d3c94c642155f77822062e849220f
ziad <ziad.nouh111@gmail.com>
2024-09-21 22:12:56 +0300

    chore: update Login & Login register to use new file names & pathes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,12 @@ const RegisterSchema = Yup.object().shape({
   password: Yup.string()
     .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
-  phoneNumber: Yup.string().required("Phone number is required"),
+  phoneNumber: Yup.string()
+    .matches(
+      /^(010|011|012|015)[0-9]{8}$/,
+      "Phone number must be a valid Egyptian number"
+    )
+    .required("Phone number is required"),
   address: Yup.string().required("Address is required"),
 });
 
