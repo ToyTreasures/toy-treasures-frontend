9fec834657f84b540ce9448fe077bababa878501
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-21 19:33:47 +0200

    fix(WoodenToys): resolve path of WoodenToysCard

--- src/Layouts/HomeLayouts/WoodenToys.jsx
+++ src/Layouts/HomeLayouts/WoodenToys.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import WoodenToysCard from "../../components/WoodenToysCard";
+import WoodenToysCard from "../WoodenToysCard";
 
 const WoodenToys = () => {
   const products = [
