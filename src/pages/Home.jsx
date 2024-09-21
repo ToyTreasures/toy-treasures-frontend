34b8b272958ceffdcbd8abf61c9ea8b8e8d94d9f
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-21 19:35:07 +0200

    style(home): update styling for Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { useRef } from "react";
 import HeroSection from "../Layouts/HomeLayouts/HeroSection";
+import StuffedAnimals from "../Layouts/HomeLayouts/StuffedAnimals";
 import ToysTypeSection from "../Layouts/HomeLayouts/ToysTypeSection";
+import WoodenToys from "../Layouts/HomeLayouts/WoodenToys";
 
 const Home = () => {
   const toysTypeSectionRef = useRef(null);
   return (
-    <div className="w-full">
+    <div className="w-full bg-gray-100">
       <HeroSection toysTypeSectionRef={toysTypeSectionRef} />
       <ToysTypeSection ref={toysTypeSectionRef} />
+      <StuffedAnimals />
+      <WoodenToys />
     </div>
   );
 };
