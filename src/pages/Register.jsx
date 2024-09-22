d6519fe130b414c689b8473fddfb46d19f05a138
ziad <ziad.nouh111@gmail.com>
2024-09-22 12:48:50 +0300

    refactore: move yup validation schemas to separate files.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { RegisterSchema } from "../utils/validatoin/userValidation";
 import registerImage from "../assets/register/r1.jpg";
 import authApiRequests from "../services/authApiRequests";
-
-const RegisterSchema = Yup.object().shape({
-  name: Yup.string().required("Full Name is required"),
-  email: Yup.string()
-    .email("Invalid email format")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .required("Password is required"),
-  phoneNumber: Yup.string()
-    .matches(
-      /^(010|011|012|015)[0-9]{8}$/,
-      "Phone number must be a valid Egyptian number"
-    )
-    .required("Phone number is required"),
-  address: Yup.string().required("Address is required"),
-});
 
 const Register = () => {
   const [registerError, setRegisterError] = useState("");
