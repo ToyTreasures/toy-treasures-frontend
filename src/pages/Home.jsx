0e04975dee271bc086ae16fb399ec95d376f9c46
AHmedEMadEG <amoemo141996@gmail.com>
2024-10-05 01:20:09 +0300

    fix: removed unexisting categories from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,7 @@ import { useRef } from "react";
 import BusinessSection from "../Layouts/HomeLayouts/BusinessSection";
 import HeroSection from "../Layouts/HomeLayouts/HeroSection";
 import Story from "../Layouts/HomeLayouts/Story";
-import StuffedAnimals from "../Layouts/HomeLayouts/StuffedAnimals";
 import CategoriesSection from "../Layouts/HomeLayouts/CategoriesSection";
-import WoodenToys from "../Layouts/HomeLayouts/WoodenToys";
 
 const Home = () => {
   const categoriesSectionRef = useRef(null);
@@ -12,8 +10,6 @@ const Home = () => {
     <div className="w-full bg-gray-100">
       <HeroSection categoriesSectionRef={categoriesSectionRef} />
       <CategoriesSection ref={categoriesSectionRef} />
-      <StuffedAnimals />
-      <WoodenToys />
       <Story />
       <BusinessSection />
     </div>
