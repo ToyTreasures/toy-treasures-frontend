15313657228d2d37f2a79417a02dda65aad4b6b5
ziad <ziad.nouh111@gmail.com>
2024-10-09 06:54:57 +0300

    feat: add confirmation modal component

---
+++ src/components/modals/ConfirmationModal.jsx
@@ -0,0 +1,41 @@
+const ConfirmationModal = ({ show, message, onConfirm, onCancel }) => {
+  if (!show) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md">
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold text-[--secondary-color]">
+            Confirm Action
+          </h2>
+          <p>{message}</p>
+        </div>
+        <div className="flex justify-end space-x-4">
+          <button
+            onClick={onCancel}
+            className="bg-gray-300 text-gray-800 hover:bg-gray-400 px-4 py-2 rounded"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onConfirm}
+            className="bg-[--primary-color] text-white hover:bg-[--secondary-color] hover:text-[--primary-color] px-4 py-2 rounded"
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
+
+// For Editing a Listed Item:
+// Message: "Do you want to save these changes to your listed item?"
+
+// For Marking an Item as Sold:
+// Message: "Are you sure you want to mark this item as sold? It will no longer be visible to other users."
+
+// For Editing User Data:
+// Message: "Are you sure you want to update your personal information?"
