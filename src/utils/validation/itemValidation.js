4e4eb745077d2327a3a1f318ea54d4ef6cad971f
ziad <ziad.nouh111@gmail.com>
2024-10-09 06:51:25 +0300

    feat: add updateitem validtion schema

--- src/utils/validation/itemValidation.js
+++ src/utils/validation/itemValidation.js
@@ -11,8 +11,8 @@ const validCategoryNames = [
 ];
 
 export const SellItemSchema = Yup.object().shape({
-  name: Yup.string().required("Item Name is required"),
-  description: Yup.string().required("Description is required"),
+  name: Yup.string().min(3).max(30).required("Item Name is required"),
+  description: Yup.string().min(3).max(500).required("Description is required"),
   price: Yup.number()
     .typeError("Price must be a number")
     .positive("Price must be a positive number")
@@ -29,6 +29,32 @@ export const SellItemSchema = Yup.object().shape({
       "fileFormat",
       "upload images only",
       (value) => value && SUPPORTED_FORMATS.includes(value.type)
+    ),
+  condition: Yup.string().required("Condition is required"),
+  category: Yup.string()
+    .oneOf(validCategoryNames, "Invalid category name")
+    .required("Category is required"),
+  isAvailableForSwap: Yup.boolean(),
+});
+
+export const UpdateItemSchema = Yup.object().shape({
+  name: Yup.string().min(3).max(30).required("Item Name is required"),
+  description: Yup.string().min(3).max(500).required("Description is required"),
+  price: Yup.number()
+    .typeError("Price must be a number")
+    .positive("Price must be a positive number")
+    .min(1)
+    .required("Price is required"),
+  image: Yup.mixed()
+    .test(
+      "fileSize",
+      "File too large",
+      (value) => !value || (value && value.size <= FILE_SIZE)
+    )
+    .test(
+      "fileFormat",
+      "Unsupported file format",
+      (value) => !value || (value && SUPPORTED_FORMATS.includes(value.type))
     ),
   condition: Yup.string().required("Condition is required"),
   category: Yup.string()
